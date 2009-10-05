a6beeba68700c6465e32316503e5e749a9e25298
Sandip Chitale <schitale@aptana.com>
2009-10-05 16:34:08 -0700

    Fix issue # 146 Sandip Chitale

--- plugins/com.aptana.ide.update.eclipse34/src/com/aptana/ide/update/AddRepositoryStartup.java
+++ plugins/com.aptana.ide.update.eclipse34/src/com/aptana/ide/update/AddRepositoryStartup.java
@@ -7,23 +7,17 @@ import org.eclipse.ui.IStartup;
 
 import org.eclipse.equinox.internal.p2.console.ProvisioningHelper;
 
-import com.aptana.ide.update.internal.manager.P2PluginManager;
-
 import com.aptana.ide.core.IdeLog;
-import com.aptana.ide.update.manager.IPluginManager;
 
 public class AddRepositoryStartup implements IStartup {
 	public void earlyStartup() {
 		// Add the db-tools update site. It is required to present when installing
 		// Cloud plugin.
 		try {
-			IPluginManager pluginManager = Activator.getDefault().getPluginManager();
-			if (pluginManager instanceof P2PluginManager) {
-				URL siteURL = new URL("http://download.aptana.org/tools/studio/plugin/install/db-tools"); //$NON-NLS-1$
-				URL[] existingMetaRepos = ProvisioningHelper.getMetadataRepositories();
-				if (!contains(existingMetaRepos, siteURL)) {
-					pluginManager.addUpdateSite(siteURL);
-				}
+			URL siteURL = new URL("http://download.aptana.org/tools/studio/plugin/install/db-tools"); //$NON-NLS-1$
+			URL[] existingMetaRepos = ProvisioningHelper.getMetadataRepositories();
+			if (!contains(existingMetaRepos, siteURL)) {
+				Activator.getDefault().getPluginManager().addUpdateSite(siteURL);
 			}
 		} catch (MalformedURLException e) {
 			IdeLog.logError(P2Activator.getDefault(), e.getMessage(), e);
