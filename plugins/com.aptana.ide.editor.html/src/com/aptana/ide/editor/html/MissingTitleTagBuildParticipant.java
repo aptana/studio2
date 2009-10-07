5af9d64dbaf65d55d9b4904de972efcf5256d060
sgtcoolguy <cwilliams@aptana.com>
2009-10-07 13:47:10 -0400

    Add build participants that check that all tag and attribute names are lowercase in XHTML, and one that makes sure tehre is always a title tag in an X/HTML file.

---
+++ plugins/com.aptana.ide.editor.html/src/com/aptana/ide/editor/html/MissingTitleTagBuildParticipant.java
@@ -0,0 +1,58 @@
+package com.aptana.ide.editor.html;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.aptana.ide.core.builder.BuildContext;
+import com.aptana.ide.core.builder.IProblem;
+import com.aptana.ide.core.builder.Warning;
+import com.aptana.ide.editor.html.parsing.nodes.HTMLElementNode;
+import com.aptana.ide.parsing.nodes.IParseNode;
+
+public class MissingTitleTagBuildParticipant extends HTMLBuildParticipant
+{
+
+	@Override
+	public void buildStarting(List<BuildContext> contexts, boolean isBatch, IProgressMonitor monitor)
+	{
+		for (BuildContext context : contexts)
+		{
+			if (!isHTMLFile(context))
+				continue;
+			// ok we have an html file
+			IParseNode root = context.getRootNode();
+			if (!containsTitleTag(context, root))
+			{
+				List<IProblem> problems = new ArrayList<IProblem>();
+				problems.add(new Warning(3, context.getFile().getFullPath().toPortableString(), -1, 0, 0,
+						"Missing 'title' tag"));
+				context.recordNewProblems(problems);
+			}
+		}
+
+	}
+
+	private boolean containsTitleTag(BuildContext context, IParseNode root)
+	{
+		IParseNode[] children = root.getChildren();
+		for (IParseNode node : children)
+		{
+			if (node instanceof HTMLElementNode)
+			{
+				HTMLElementNode elementNode = (HTMLElementNode) node;
+				String name = elementNode.getName();
+				if (name.equalsIgnoreCase("title"))
+					return true;
+				if (name.equalsIgnoreCase("body"))
+					return false;
+			}
+			boolean containsTitleTag = containsTitleTag(context, node);
+			if (containsTitleTag)
+				return true;
+		}
+		return false;
+	}
+
+}
