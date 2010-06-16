24296d8b7e5962d4b9c95c7c7be88db1ecd37ce7
mxia <mxia@aptana.com>
2010-06-16 14:51:57 -0700

    Added first_run to the ping data in 2.0.

--- plugins/com.aptana.ide.core.ui/src/com/aptana/ide/core/ui/ping/PingStartup.java
+++ plugins/com.aptana.ide.core.ui/src/com/aptana/ide/core/ui/ping/PingStartup.java
@@ -374,6 +374,10 @@ public class PingStartup implements IStartup
 		preferences.loadPreferences();
 
 		// build key/value pairs
+		if (!preferences.getBoolean(IPreferenceConstants.P_IDE_HAS_RUN))
+		{
+			addKeyPair(keyValues, "first_run", ""); //$NON-NLS-1$ //$NON-NLS-2$
+		}
 		addKeyPair(keyValues, "id", getApplicationId(preferences)); //$NON-NLS-1$
 		addKeyPair(keyValues, "version", PluginUtils.getPluginVersion(AptanaCorePlugin.getDefault())); //$NON-NLS-1$
 		addKeyPair(keyValues, "product", System.getProperty("eclipse.product")); //$NON-NLS-1$ //$NON-NLS-2$
